6403ee8b3684312b56e3b2ee941c10165199edbb
Cory Streiff <c.streiff@pm.me>
2025-06-06 12:03:05 +0200

    feat: add session id state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
+import { useRef, useState } from "react";
+// import { Peer } from "peerjs";
 import "./App.css";
 
 function App() {
-  const uuid = crypto.randomUUID();
+  const clientId = useRef(crypto.randomUUID());
 
+  const [text, setText] = useState<string>("");
+  const [sessionId, setSessionId] = useState<string | null>(null);
+
+  // const peer = new Peer(sessionId as UUID);
   return (
     <>
       <h1>Peer to Peer</h1>
-      <div>client UUID: {uuid}</div>
-      <div className="card"></div>
+      <div>client UUID: {clientId.current}</div>
+      <div>session id: {sessionId}</div>
+      <h2>Create a session</h2>
+      <p>Either paste a session in or create your own and share it</p>
+      <input onChange={(e) => setText(e.target.value)}></input>
+      <button onClick={() => setSessionId(text)}>Submit</button>
     </>
   );
 }
